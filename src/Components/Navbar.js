bbf07ab24082f7f7b008bd1cbf33d198a987eb53
Ubaid Ur Rehman <119524246+UBAID0315@users.noreply.github.com>
2024-08-15 08:15:28 +0500

    making actions and reducers

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Redux Application</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>            
+                <button className='btn btn-primary mx-2 my-2'>Your balance is: 10000</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/Components/Shop.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export const Shop = () => {
+    return (
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+                <div className="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf6eluXzxsKXp3MLH4OhirwCrPm2Ivc4hxGQ&usqp=CAU" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Orange</h5>
+                            <p className="card-text">Get a burst of sunshine in every bite with our fresh, juicy oranges—picked at the peak of ripeness for a naturally sweet and tangy flavor!</p>
+                            <button className='btn btn-primary mx-4'>-</button>
+                            1
+                            <button className='btn btn-primary mx-4'>+</button>
+                        </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGf7scTI1fwqlwr8ybL6vvQv7MxaZaYe52jA&usqp=CAU" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Strawberry</h5>
+                            <p className="card-text">Indulge in the sweetness of summer with our vibrant, juicy strawberries—perfectly ripe and bursting with natural flavor!</p>
+                            <button className='btn btn-primary mx-4'>-</button>
+                            1
+                            <button className='btn btn-primary mx-4'>+</button>
+                        </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDLMlIIsJEmaHMA0cpAaOcjbliIyjkfjhg6g&usqp=CAU" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Apple</h5>
+                            <p className="card-text">Crisp, juicy, and full of flavor—our apples are the perfect snack to keep you refreshed and energized all day long!</p>
+                            <button className='btn btn-primary mx-4'>-</button>
+                            1
+                            <button className='btn btn-primary mx-4'>+</button>
+                        </div>
+                </div>
+            </div>            
+        </div>
+    )
+}
+
+export default Shop
